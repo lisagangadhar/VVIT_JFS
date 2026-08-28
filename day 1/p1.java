55a17e10bd68aea3a51a080e8ea641230d7b79c1
lisa_gangadhar <monalisagowda6107@gmail.com>
2026-08-28 10:32:11 +0530

    added remaining java files day 1

---
+++ day 1/p1.java
@@ -0,0 +1,15 @@
+//variables in java 
+public class p1{
+    public static void main(String[] args){
+        short shortnumber = 32000;
+        int age = 25;
+
+        float temperature = 36.6f;
+        double price = 99.99;
+        char grade = 'A';
+        boolean isEnrolled = true;
+
+        String studentname = "monalisa";
+        System.out.println("Student : " + studentname);
+    }
+}
